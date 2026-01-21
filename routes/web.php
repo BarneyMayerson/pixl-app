b8504650ecbf05a7d7762cc611b328fb6384914e
Barney Mayerson <mr.barney.mayerson@gmail.com>
2026-01-21 13:02:19 +0300

    Creating  feed Item

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,48 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('welcome'));
+Route::get('/', fn () => view('welcome'));
 
-Route::get('/feed', fn() => view('feed'));
+Route::get('/feed', function () {
+    $feedItems = json_decode(json_encode([
+        [
+            'postedDateTime' => '3h',
+            'content' => <<<'str'
+ <p>I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a></p>
+<img src="/storage/images/simon-chilling.png" alt="" />
+str,
+            'likeCount' => 23,
+            'replyCount' => 45,
+            'repostCount' => 151,
+            'profile' => [
+                'avatar' => '/storage/images/michael.png',
+                'displayName' => 'Michael',
+                'handle' => '@mmich_jj',
+            ],
+        ],
+    ]));
 
-Route::get('/profile', fn() => view('profile'));
+    return view('feed', compact('feedItems'));
+});
+
+Route::get('/profile', function () {
+    $feedItems = json_decode(json_encode([
+        [
+            'postedDateTime' => '3h',
+            'content' => <<<'str'
+ <p>I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a></p>
+<img src="/storage/images/simon-chilling.png" alt="" />
+str,
+            'likeCount' => 23,
+            'replyCount' => 45,
+            'repostCount' => 151,
+            'profile' => [
+                'avatar' => '/storage/images/michael.png',
+                'displayName' => 'Michael',
+                'handle' => '@mmich_jj',
+            ],
+        ],
+    ]));
+
+    return view('profile', compact('feedItems'));
+});
